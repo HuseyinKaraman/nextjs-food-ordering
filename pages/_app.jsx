24aa0e9a0e31e13cd5f3aa198207e3f1ec690f57
Hercules <hsy.krmn@gmail.com>
2024-01-06 20:11:45 +0300

    Add nprogress Bar

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -8,8 +8,17 @@ import store from "../redux/store";
 import { Provider } from "react-redux";
 import { SessionProvider } from "next-auth/react";
 
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+Router.events.on("routeChangeStart", () =>NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
 
 
 // ** const router = useRouter(); admin layout eklersek bunu kullanabiliriz!
