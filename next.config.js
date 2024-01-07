ce5e77ac61584d5b7b0e000a5e929ec6ffd21c1b
Hercules <hsy.krmn@gmail.com>
2024-01-07 21:48:32 +0300

    refactoring

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   images : {
     domains: ["res.cloudinary.com"]
-  }
+  },
+  experimental: {
+    appDir: false,
+  },
 }
 
 module.exports = nextConfig
